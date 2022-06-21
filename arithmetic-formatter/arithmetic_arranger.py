27b6d041244e12ec5a3f505a64199483678e7649
murilodevv <murilomachadob1@gmail.com>
2022-06-21 14:14:53 -0300

    Last Modifications

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,14 +1,16 @@
 def arithmetic_arranger(problems, show = False):
+
+  # Variables
   first = ""
   op = ""
   dashes = ""
   second = ""
   sumup = ""
-  answer = ""
+  arithmetic_arranger = ""
   sum = ""
+  len_problems = len(problems)
 
-
-  if(len(problems) > 5):
+  if len_problems > 5:
     return "Error: Too many problems."
 
   for problem in problems:
@@ -16,10 +18,13 @@ def arithmetic_arranger(problems, show = False):
     op = problem.split(" ")[1]
     second_num = problem.split(" ")[2]
 
-    if (len(first_num) > 4 or len(second_num) > 4):
+    len_first = len(first_num)
+    len_second = len(second_num)
+    
+    if len_first > 4 or len_second > 4:
       return "Error: Numbers cannot be more than four digits."
 
-    if not first_num.isnumeric() or not second_num.isnumeric():
+    if not first_num.isdigit() or not second_num.isdigit():
       return "Error: Numbers must only contain digits."
     
     if (op == '+' or op == '-'):
@@ -33,26 +38,27 @@ def arithmetic_arranger(problems, show = False):
     length = max(len(first_num) , len(second_num)) + 2
     first_line = str(first_num).rjust(length)
     second_line = op + str(second_num.rjust(length - 1))
-    sltn = str(sum.rjust(length))
+    sum_line = str(sum.rjust(length))
     
    
     dash_line = ""
-    for dash in range(length):
+    for d in range(length):
       dash_line += "-"
 
     if problem != problems[-1]: 
       first += first_line + '    '
       second += second_line + '    '
       dashes += dash_line + '    '
-      sumup += sltn + '    '
+      sumup += sum_line + '    '
     else: 
       first += first_line
       second += second_line 
       dashes += dash_line 
-      sumup += sltn  
+      sumup += sum_line  
 
   if show: 
-    answer = first + "\n" + second + "\n" + dashes + "\n" + sumup
+    arithmetic_arranger = first + "\n" + second + "\n" + dashes + "\n" + sumup
   else:
-    answer = first + "\n" + second + "\n" + dashes    
-  return answer
+    arithmetic_arranger = first + "\n" + second + "\n" + dashes    
+    
+  return arithmetic_arranger
